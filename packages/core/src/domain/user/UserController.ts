2d2b9a139c9a2ed3d22e403de1c5642027121801
Mikhail Sokolov <debagger@gmail.com>
2022-11-02 00:25:57 +0500

    Поправил работу формы профиля

--- packages/core/src/domain/user/UserController.ts
+++ packages/core/src/domain/user/UserController.ts
@@ -1,10 +1,30 @@
 import { IUser } from './IUser'
 import { IUserApi } from './IUserApi'
 import { UserService } from './UserService'
+import { UpdateUserData } from './IUserPort'
+import {
+    ControllerType,
+    IControllerMethodInput,
+    IControllerMethodOutput,
+} from '../../IController'
+import { right } from '@sweet-monads/either'
 
-export class UserController implements IUserApi {
+export class UserController implements ControllerType<UserController> {
     constructor(private readonly userService: UserService) {}
-    getUser(email: string): Promise<IUser> {
-        throw new Error('Method not implemented.')
+    async getUser(
+        input: IControllerMethodInput<{ email: string }>
+    ): IControllerMethodOutput<IUser> {
+        return right(await this.userService.getUserByEmail(input.data.email))
+    }
+
+    async updateUser({
+        data,
+    }: IControllerMethodInput<{
+        email: string
+        data: UpdateUserData
+    }>): IControllerMethodOutput<IUser> {
+        const { data: newData, email } = data
+        const user = await this.userService.updateUser(email, newData)
+        return user
     }
 }
