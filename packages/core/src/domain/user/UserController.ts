1dd49715697dc73071a00f2a862833d17b4f4c96
Mikhail Sokolov <debagger@gmail.com>
2022-11-02 00:34:29 +0500

    Добавлен метод api getCurrent

--- packages/core/src/domain/user/UserController.ts
+++ packages/core/src/domain/user/UserController.ts
@@ -5,7 +5,7 @@ import { UpdateUserData } from './IUserPort'
 import {
     ControllerType,
     IControllerMethodInput,
-    IControllerMethodOutput,
+    ControllerMethodOutput,
 } from '../../IController'
 import { right } from '@sweet-monads/either'
 
@@ -13,8 +13,15 @@ export class UserController implements ControllerType<UserController> {
     constructor(private readonly userService: UserService) {}
     async getUser(
         input: IControllerMethodInput<{ email: string }>
-    ): IControllerMethodOutput<IUser> {
+    ): ControllerMethodOutput<IUser> {
         return right(await this.userService.getUserByEmail(input.data.email))
+    }
+
+    async getCurrent({
+        context,
+    }: IControllerMethodInput<void>): ControllerMethodOutput<IUser> {
+        const user = await this.userService.getUserByEmail(context.email)
+        return right(user)
     }
 
     async updateUser({
@@ -22,7 +29,7 @@ export class UserController implements ControllerType<UserController> {
     }: IControllerMethodInput<{
         email: string
         data: UpdateUserData
-    }>): IControllerMethodOutput<IUser> {
+    }>): ControllerMethodOutput<IUser> {
         const { data: newData, email } = data
         const user = await this.userService.updateUser(email, newData)
         return user
