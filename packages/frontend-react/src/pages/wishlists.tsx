46a60b806a2d5e53047524b3cee787e06e6194ce
kolombo306 <gruzintsev@ro.ru>
2022-11-08 14:30:42 +0300

    wishlist template v3

--- packages/frontend-react/src/pages/wishlists.tsx
+++ packages/frontend-react/src/pages/wishlists.tsx
@@ -1,11 +1,106 @@
-import React from 'react'
+import * as React from 'react'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
-const Wishlists = () => {
+const wishlistArray = [
+    {
+        wishlistName: 'PC and video',
+        items: [
+            {
+                itemName: 'TV',
+                price: 25000,
+                plannedFlow: '?',
+            },
+            {
+                itemName: 'Playstation 5',
+                price: 55000,
+                plannedFlow: '?',
+            },
+            {
+                itemName: 'RTX 4090',
+                price: 155000,
+                plannedFlow: '?',
+            },
+        ],
+    },
+    {
+        wishlistName: 'Relax',
+        items: [
+            {
+                itemName: 'Hookah',
+                price: 11000,
+                plannedFlow: '?',
+            },
+            {
+                itemName: 'Home lamp',
+                price: 5000,
+                plannedFlow: '?',
+            },
+            {
+                itemName: 'Soft chair',
+                price: 15000,
+                plannedFlow: '?',
+            },
+        ],
+    },
+]
+
+export default function Wishlists() {
     return (
-        <>
-            <h1>Wishlists template</h1>
-        </>
+        <div>
+            {wishlistArray.map((el) => (
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>{el.wishlistName}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Item&nbsp;name</TableCell>
+                                        <TableCell align="right">Price,&nbsp;RUB</TableCell>
+                                        <TableCell align="right">Planned&nbsp;flow</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {el.items.map((row) => (
+                                        <TableRow
+                                            key={row.itemName}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row">
+                                                {row.itemName}
+                                            </TableCell>
+                                            <TableCell align="right">{row.price}</TableCell>
+                                            <TableCell align="right">{row.plannedFlow}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+            <Accordion disabled>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel3a-content" id="panel3a-header">
+                    <Typography>Inactive wishlist</Typography>
+                </AccordionSummary>
+            </Accordion>
+        </div>
     )
 }
-
-export default Wishlists
